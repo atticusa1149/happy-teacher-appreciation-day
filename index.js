da519443c2bf2521df94dfc105ee988d287f1a4e
atticusa1149 <the13mar@gmail.com>
2025-05-09 15:20:37 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const right=()=>{document.getElementById('answer1').innerText="your answer is correct";
+    document.getElementById('answer2').innerText="";
+}
+const wrong=()=>{document.getElementById('answer2').innerText="your answer is wrong";
+    document.getElementById('answer1').innerText='';
+}
